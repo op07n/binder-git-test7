7147824fe02bb20810a2bcd33ca23a8973a8a5b0
op07n <39182105+op07n@users.noreply.github.com>
2020-01-09 15:48:32 +0100

    Update streamlit_call.py

--- .binder/streamlit_call.py
+++ .binder/streamlit_call.py
@@ -1,7 +1,9 @@
 from subprocess import Popen
 import os
+import sys
 
 def load_jupyter_server_extension(nbapp):
     """serve the streamlit app"""
     os.chdir('rhyme-with-ai/app/')
+    sys.path.insert(0, "rhyme-with-ai/src/rhyme_with_ai")
     Popen(["streamlit", "run", "app.py", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False"])
