8ca8685cb4189a9cdac9aac729630fb1e178bf47
op07n <39182105+op07n@users.noreply.github.com>
2020-01-10 14:28:27 +0100

    Update streamlit_call.py

--- .binder/streamlit_call.py
+++ .binder/streamlit_call.py
@@ -4,4 +4,4 @@ import os
 def load_jupyter_server_extension(nbapp):
     """serve the streamlit app"""
     os.chdir('rhyme-with-ai/app/')
-    Popen(["PYTHONPATH=$PYTHONPATH:src/rhyme_with_ai/\ streamlit", "run", "app.py", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False"])
+    Popen(["PYTHONPATH=$PYTHONPATH:/home/jovyan/rhyme-with-ai/src/rhyme_with_ai/\ streamlit", "run", "app.py", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False"])
